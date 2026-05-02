ba01acfe1c892a3bf497f9207f8f970ace8e893c
numberw8835 <numberw8835@gmail.com>
2026-05-02 14:08:08 +0300

    fix: increase charset buffer size from 100 to 128 to prevent potential buffer overflow

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ const char numbers[] = "0123456789";
 const char special[] = "!@#$%^&*()_-+={}[]|;:,.<>?";
 
 void generate_password(int size, int use_lower, int use_upper, int use_num, int use_spec) {
-    char charset[100] = "";
+    char charset[128] = "";
     if (use_lower) strcat(charset, lowercase);
     if (use_upper) strcat(charset, uppercase);
     if (use_num) strcat(charset, numbers);
@@ -39,6 +39,23 @@ void generate_password(int size, int use_lower, int use_upper, int use_num, int 
 
 int main(int argc, char const *argv[])
 {
+    int has_h = 0;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
+            has_h = 1;
+        }
+    }
+    if (has_h) {
+        printf("Usage: %s <size> [-nn] [-ns] [-nl] [-nu] [-h]\n", argv[0]);
+        printf("Options:\n");
+        printf("  -nn  No numbers\n");
+        printf("  -ns  No special characters\n");
+        printf("  -nl  No lowercase letters\n");
+        printf("  -nu  No uppercase letters\n");
+        printf("  -h   Show this help message\n");
+        exit(0);
+    }
+
     if (argc < 2) {
         printf("Usage: %s <size> [-nn] [-ns] [-nl] [-nu]\n", argv[0]);
         exit(1);
@@ -53,7 +70,16 @@ int main(int argc, char const *argv[])
     int use_lower = 1, use_upper = 1, use_num = 1, use_spec = 1;
 
     for (int i = 2; i < argc; i++) {
-        if (strcmp(argv[i], "-nn") == 0) {
+        if (strcmp(argv[i], "-h") == 0) {
+            printf("Usage: %s <size> [-nn] [-ns] [-nl] [-nu] [-h]\n", argv[0]);
+            printf("Options:\n");
+            printf("  -nn  No numbers\n");
+            printf("  -ns  No special characters\n");
+            printf("  -nl  No lowercase letters\n");
+            printf("  -nu  No uppercase letters\n");
+            printf("  -h   Show this help message\n");
+            exit(0);
+        } else if (strcmp(argv[i], "-nn") == 0) {
             use_num = 0;
         } else if (strcmp(argv[i], "-ns") == 0) {
             use_spec = 0;
